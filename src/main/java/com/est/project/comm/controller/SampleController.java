225fb3fca325e85ba7650f4b0c060b7166585dd8
DESKTOP-AN1016U\skaeo <skaeotjd0@gmail.com>
2024-04-17 14:18:41 +0900

    CommonSample "first commit" merge 받아주세요.

--- src/main/java/com/est/project/comm/controller/SampleController.java
+++ src/main/java/com/est/project/comm/controller/SampleController.java
@@ -3,16 +3,10 @@ package com.est.project.comm.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+
 public class SampleController {
 
 	
-	@GetMapping("/log")
-	public String name() {
-		return "common/log";
-	}
-	@GetMapping("/log")
-	public String assa() {
-		return "common/log";
-	}
+
+
 }
